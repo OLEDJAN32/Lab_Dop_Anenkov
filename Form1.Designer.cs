7f0131dc575114d0336e26fb0de254f462915c00
Anenkov_Oleg <anenkovoleg04@gmail.com>
2024-04-02 15:48:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Lab_Dop_Anenkov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonCompleted = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Node = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonCompleted);
+            this.tabPage1.Controls.Add(this.buttonDelete);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(768, 397);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Заметки";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonCompleted
+            // 
+            this.buttonCompleted.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCompleted.Location = new System.Drawing.Point(364, 355);
+            this.buttonCompleted.Name = "buttonCompleted";
+            this.buttonCompleted.Size = new System.Drawing.Size(401, 39);
+            this.buttonCompleted.TabIndex = 2;
+            this.buttonCompleted.Text = "Пометить как выполненное";
+            this.buttonCompleted.UseVisualStyleBackColor = true;
+            this.buttonCompleted.Click += new System.EventHandler(this.buttonCompleted_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(3, 355);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(250, 39);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Удалить задачу";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Node,
+            this.Date,
+            this.Time,
+            this.Result});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(768, 350);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Node
+            // 
+            this.Node.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Node.HeaderText = "Задача";
+            this.Node.MinimumWidth = 6;
+            this.Node.Name = "Node";
+            this.Node.ReadOnly = true;
+            // 
+            // Date
+            // 
+            this.Date.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Date.HeaderText = "Дата выполнения";
+            this.Date.MinimumWidth = 6;
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // Time
+            // 
+            this.Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Time.HeaderText = "Время выполнения";
+            this.Time.MinimumWidth = 6;
+            this.Time.Name = "Time";
+            this.Time.ReadOnly = true;
+            // 
+            // Result
+            // 
+            this.Result.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Result.HeaderText = "Состояние";
+            this.Result.MinimumWidth = 6;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.dateTimePicker2);
+            this.tabPage2.Controls.Add(this.buttonAdd);
+            this.tabPage2.Controls.Add(this.dateTimePicker1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 397);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Добавить задачу";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(428, 144);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(131, 36);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.AutoSize = true;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(259, 228);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(263, 68);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(221, 144);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(172, 36);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(7, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(755, 36);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(286, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите задачу:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(216, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(343, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите сроки выполнения:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonCompleted;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Node;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Xml.Linq;
+
+namespace Lab_Dop_Anenkov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Database();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Database()
+        {
+            using (StreamReader reader = new StreamReader("data.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ');
+                    Add(words[0], words[1], words[2], words[3]);
+                }
+                reader.Close();
+            }
+        }
+
+        private void Add(string node, string date, string time, string complete)
+        {
+            var index = this.dataGridView1.Rows.Add();
+            this.dataGridView1.Rows[index].Cells[0].Value = node;
+            this.dataGridView1.Rows[index].Cells[1].Value = date;
+            this.dataGridView1.Rows[index].Cells[2].Value = time;
+            this.dataGridView1.Rows[index].Cells[3].Value = complete;
+            if(complete!="Выполнено")
+            {
+                checktime(index);
+            }
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="")
+            {
+                return;
+            }
+
+            string textToAdd = textBox1.Text;
+            var index = this.dataGridView1.Rows.Add();
+            this.dataGridView1.Rows[index].Cells[0].Value=textToAdd;
+
+            string textDate = dateTimePicker1.Text;
+            string textTime = dateTimePicker2.Text;
+            this.dataGridView1.Rows[index].Cells[1].Value = textDate;
+            this.dataGridView1.Rows[index].Cells[2].Value = textTime;
+            checktime(index);
+            SaveDataToFile();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.Rows.Count>1)
+            {
+                int delet = dataGridView1.SelectedCells[0].RowIndex;
+                dataGridView1.Rows.RemoveAt(delet);
+            }
+            SaveDataToFile();
+        }
+
+        private void checktime(int index)
+        {
+            DateTime currentDateAndTime = DateTime.Now;
+            DateTime dateToCompare;
+            DateTime timeToCompare;
+            string inputDate, inputTime;
+
+            DataGridViewCell cellData = dataGridView1.Rows[index].Cells[1];
+            DataGridViewCell cellTime = dataGridView1.Rows[index].Cells[2];
+            inputDate = cellData.Value.ToString();
+            inputTime = cellTime.Value.ToString();
+            dateToCompare = DateTime.ParseExact(inputDate, "dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            timeToCompare = DateTime.ParseExact(inputTime, "HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+            if (dateToCompare.Date > currentDateAndTime.Date)
+            {
+                this.dataGridView1.Rows[index].Cells[3].Value = "Выполняется";
+                return;
+            }
+            else
+            {
+                if (dateToCompare.Date == currentDateAndTime.Date && timeToCompare.TimeOfDay >= currentDateAndTime.TimeOfDay)
+                {
+                    this.dataGridView1.Rows[index].Cells[3].Value = "Выполняется";
+                    return;
+                }
+                else
+                {
+                    this.dataGridView1.Rows[index].Cells[3].Value = "Просрочено";
+                }
+            }
+        }
+
+        private void buttonCompleted_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.SelectedCells[0].RowIndex;
+            if(this.dataGridView1.Rows[index].Cells[1].Value!=null)
+            {
+                this.dataGridView1.Rows[index].Cells[3].Value = "Выполнено";
+            }
+            SaveDataToFile();
+        }
+
+        private void SaveDataToFile()
+        {
+            DataGridViewCell cellNode, cellDate, cellTime, cellComplete;
+            string node, date, time, complete;
+            StreamWriter writer = new StreamWriter("data.txt");
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                cellNode = dataGridView1.Rows[i].Cells[0];
+                cellDate = dataGridView1.Rows[i].Cells[1];
+                cellTime = dataGridView1.Rows[i].Cells[2];
+                cellComplete = dataGridView1.Rows[i].Cells[3];
+
+                node = cellNode.Value.ToString();
+                date = cellDate.Value.ToString();
+                time = cellTime.Value.ToString();
+                complete = cellComplete.Value.ToString();
+
+                writer.Write(node + " ");
+                writer.Write(date + " ");
+                writer.Write(time + " ");
+                writer.Write(complete + "\n");
+            }
+            writer.Close();
+        }
+    }
+}
